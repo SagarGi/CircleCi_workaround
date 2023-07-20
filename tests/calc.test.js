6ba8c42ea99d311c8a1bd8a29c396bf4e7b051c2
sagar <sagargurung1001@gmail.com>
2023-07-20 21:57:06 +0545

    Added test case

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -8,5 +8,8 @@ describe("calculator ", () => {
     test("sum of 1 + 2 should not be equla to 4", () => {
       expect(sum(1, 2)).not.toBe(4);
     });
+    test("sum of 2 + 2 equlas 4", () => {
+      expect(sum(2, 2)).toBe(4);
+    });
   });
 });
