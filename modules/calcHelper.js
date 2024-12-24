8405ddf52ae325d5d9a53a2abf83eacb605d158c
Sagar <sagargurung1001@gmail.com>
2024-12-24 15:14:29 +0545

    naya feature
    Signed-off-by: Sagar <sagargurung1001@gmail.com>

--- modules/calcHelper.js
+++ modules/calcHelper.js
@@ -6,17 +6,17 @@ function subtract(a, b) {
   return a - b;
 }
 
-function times(a, b) {
-  return a * b;
-}
-
 function divide(a, b) {
   return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
 }
 
 module.exports = {
   sum,
   subtract,
-  times,
+  multiply,
   divide,
 };
